31c1086528e3f69583d2814a2c7e2e0837a21cfc
Aadesh Bagmar <aadeshbagmar@gmail.com>
2018-11-01 17:57:06 +0530

    Demo - investment profiles

--- examples/investment_portfolio.py
+++ examples/investment_portfolio.py
@@ -5,8 +5,56 @@
 import argparse
 import pandas as pd
 
-if __name__ == '__main__':
+from typing import List
 
+
+def extract_investments(args)->pd.DataFrame:
+
+    df = pd.read_csv("Shark Tank Companies.csv")
+    df["all_sharks"] = df["shark1"] + df["shark2"] + df["shark3"] + df["shark4"] + df["shark5"]
+
+    if args.deal_offered:
+        df = df[df["deal"]]
+
+    if args.mark_cuban:
+        df = df[df.all_sharks.str.contains("Mark Cuban")]
+
+    if args.kevin_leary:
+        df = df[df.all_sharks.str.contains("Kevin Leary")]
+
+    if args.barabara_corcoran:
+        df = df[df.all_sharks.str.contains("Mark Cuban")]
+
+    if args.lori_greiner:
+        df = df[df.all_sharks.str.contains("Kevin Leary")]
+
+    if args.robert_herjavec:
+        df = df[df.all_sharks.str.contains("Mark Cuban")]
+
+    if args.daymond_johnson:
+        df = df[df.all_sharks.str.contains("Kevin Leary")]
+
+    if args.category:
+        df = df[df.category.str.contains(args.category)]
+
+    if args.company_title:
+        df = df[df.title.str.contains(args.company_title)]
+
+    if args.season != 0:
+        df = df[df.season == args.season]
+
+    if args.minimum_valuation:
+        df = df[df.valuation >= args.minimum_valuation]
+
+    if args.max_results:
+        df = df.head(args.max_results)
+
+    return df
+
+
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     # Checking the boolean type
@@ -20,13 +68,23 @@ if __name__ == '__main__':
     parser.add_argument('--daymond-johnson', help="Daymond Johnson was present", type=bool, default=False)
 
     # Checking the string type. No default value or required
-    parser.add_argument('--category', help="Enter the category", type=str)
+    parser.add_argument('--category', help="Enter the category (Substring match)", type=str)
+    parser.add_argument('--company-title', help="Enter the name of the company (Substring match)", type=str,
+                        required=False)
 
     # Checking the integer type
-    parser.add_argument('--season', help="Enter the shark tank season", type=int, default=5, required=True)
+    parser.add_argument('--season', help="Enter the shark tank season. Enter -1 for all seasons", type=int, default=-1)
+
+    parser.add_argument('--max-results', help="Maximum number of results to be displayed", type=int,
+                        required=True)
+
 
     # Checking the float type check with default value
     parser.add_argument('--minimum-valuation', help="Enter the minimum valuation", type=float, default=1000000.00)
 
+
     args = parser.parse_args()
     print(args)
+    print(extract_investments(args))
+
+
