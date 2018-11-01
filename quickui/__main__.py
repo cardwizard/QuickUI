a569bcbc8348a487f18966ae89617f0f00679e71
Aadesh Bagmar <aadesh@soroco.com>
2018-11-01 21:27:39 +0530

    Adding an uploader buffer

--- quickui/__main__.py
+++ quickui/__main__.py
@@ -10,28 +10,21 @@ from quickui.web_builder import app, socketio
 import os
 
 def main():
+
+    port_number = os.environ.get("PORT", 8756)
+
     if len(sys.argv) < 2:
-        print("Please enter your file name separated by space. e.g.")
-        print("python -m quickui cool-python-file.py")
+        print("Launching the uploader page")
+        socketio.run(app, port_number)
         return
 
     file_path = sys.argv[1]
-    print(file_path)
-    print(os.getcwd())
 
     ea = ExtractArgs(file_path)
     config = Config(ea.find_args(), file_path)
     app.config.from_object(config)
 
-
-    if len(sys.argv) > 2:
-        port_number = sys.argv[2]
-    else:
-        port_number = os.environ.get("PORT", 8756)
-    print(port_number)
-
     socketio.run(app, port=port_number)
-
 
 if __name__ == '__main__':
     main()
